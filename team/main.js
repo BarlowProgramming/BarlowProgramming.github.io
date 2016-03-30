bd8b3c608385c4fe2f53dbca5a42e87e18649536
Jonathan Lam <jlam55555@gmail.com>
2016-03-29 23:46:48 -0400

    Added user details functionality

--- team/main.js
+++ team/main.js
@@ -14,17 +14,30 @@ var $userList = $('#users-list')
 function displayUsers(users) {
   for (var i = 0; i < users.length; i++) {
     var user = users[i]
-    // console.log(user.name);
     var $userLi = $(
-      `<a class="user-container animate" href="${user.html_url}">
-        <img class="user-picture" src="${user.avatar_url}" alt="${user.login}'s profile picture"/>
-        <div class="user-info">
-          <span class="user-link" href="${user.html_url}">${user.login}</span>
-        </div>
-      </a>`
-    )
+      `<a id="user-${user.login}" class="user-container animate" href="${user.html_url}">
+         <div class="user-main-container">
+	       <img class="user-picture" src="${user.avatar_url}" alt="${user.login}'s profile picture"/>
+           <div class="user-info">
+             <span class="user-link" href="${user.html_url}">${user.login}</span>
+           </div>
+		 </div>
+		 <div class="user-details-container">
+		   <span class="user-link"></span>	
+		 </div>
+       </a>`
+    );
     $userList.append($userLi)
   }
+  $.getJSON("userDetails.json", function(userDetails) {
+    for(var i = 0; i < userDetails.length; i++) {
+      if($("#user-" + userDetails[i].username).length == 1) {
+	    var userContainer = $("#user-" + userDetails[i].username);
+  	    userContainer.find(".user-link:last").text(userDetails[i].name);
+	    userContainer.find(".user-details-container").append(userDetails[i].details || "<div style='text-align:center'>No details</div>");
+	  }
+    }
+  });
   $("a.user-container").hover(
     function() {
       var current = $(this).addClass("big");
@@ -32,8 +45,11 @@ function displayUsers(users) {
 	  var userContainers = $("a.user-container:not(.big)");
 	  userContainers.eq(row*3).addClass("small");
 	  userContainers.eq(row*3+1).addClass("small");
+	  current.children().addClass("active");
     }, function() {
-      $("a.user-container").removeClass("small").removeClass("big");
+      $("a.user-container:not(big)").removeClass("small");
+	  $(this).removeClass("big");
+	  $(this).children().removeClass("active");
     }	
   );
 }
